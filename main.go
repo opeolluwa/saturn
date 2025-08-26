65fc8dc32c234fd4b8cea70e0356c40894a13b4b
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-08-26 14:22:13 +0100

    update

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"github.com/opeolluwa/saturn/router"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello Backend Sprint")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	router.LoadRoutes(e)
 }
